be18826202f681ae44b9db44459cb8c7f9ed3e5c
rickygomesPT <59871933+rickygomesPT@users.noreply.github.com>
2025-10-07 13:15:37 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+// ===== Funções da página inicial =====
+
+function createRoom() {
+  const creator = document.getElementById('creatorName').value.trim();
+  const roomName = document.getElementById('roomName').value.trim();
+  const roomCode = document.getElementById('roomCode').value.trim();
+  const roomTimeMin = parseInt(document.getElementById('roomTime').value) || 10;
+
+  if (!creator || !roomName || !roomCode) return alert("Preencha todos os campos.");
+
+  const roomData = {
+    roomName: roomName,
+    creator: creator,
+    createdAt: Date.now(),
+    roomDuration: roomTimeMin * 60 * 1000
+  };
+
+  localStorage.setItem("roomData", JSON.stringify(roomData));
+  localStorage.setItem("roomCode", roomCode);
+  localStorage.setItem("userName", creator);
+
+  window.location.href = "./chat.html";
+}
+
+function joinRoom() {
+  const user = document.getElementById('userName').value.trim();
+  const roomCode = document.getElementById('joinRoomCode').value.trim();
+  if (!user || !roomCode) return alert("Preencha todos os campos.");
+
+  localStorage.setItem("roomCode", roomCode);
+  localStorage.setItem("userName", user);
+  window.location.href = "./chat.html";
+}
